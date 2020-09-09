c74bad2096cf3b591771884b07e348f8c631dde8
Jeff Wendling <leterip@gmail.com>
2020-09-09 13:02:58 -0400

    handle eintr

--- filesystem/file.go
+++ filesystem/file.go
@@ -2,7 +2,6 @@ package filesystem
 
 import (
 	"io"
-	"os"
 	"syscall"
 
 	"github.com/zeebo/errs"
@@ -12,29 +11,45 @@ type File struct {
 	fd uintptr
 }
 
+func (fh File) Fd() int {
+	return int(fh.fd)
+}
+
 func (fh File) Close() (err error) {
-	return errs.Wrap(syscall.Close(int(fh.fd)))
+intr:
+	err = syscall.Close(int(fh.fd))
+	if err == syscall.EINTR {
+		goto intr
+	}
+	return errs.Wrap(err)
 }
 
 func (fh File) Write(p []byte) (n int, err error) {
+intr:
 	n, err = syscall.Write(int(fh.fd), p)
-	return n, errs.Wrap(err)
-}
-
-func (fh File) Read(p []byte) (n int, err error) {
-	n, err = syscall.Read(int(fh.fd), p)
-	if err == nil && n == 0 {
-		return 0, io.EOF
+	if err == syscall.EINTR {
+		goto intr
 	}
 	return n, errs.Wrap(err)
 }
 
-var _ = (*os.File).ReadAt
+func (fh File) Read(p []byte) (n int, err error) {
+intr:
+	n, err = syscall.Read(int(fh.fd), p)
+	if err == nil && n == 0 {
+		return 0, io.EOF
+	} else if err == syscall.EINTR {
+		goto intr
+	}
+	return n, errs.Wrap(err)
+}
 
 func (fh File) ReadAt(p []byte, off int64) (n int, err error) {
 	for len(p) > 0 {
 		m, err := syscall.Pread(int(fh.fd), p, off)
-		if err != nil {
+		if err == syscall.EINTR {
+			continue
+		} else if err != nil {
 			return n, errs.Wrap(err)
 		} else if m == 0 {
 			return n, io.EOF
@@ -47,28 +62,49 @@ func (fh File) ReadAt(p []byte, off int64) (n int, err error) {
 }
 
 func (fh File) Seek(offset int64, whence int) (off int64, err error) {
+intr:
 	off, err = syscall.Seek(int(fh.fd), offset, whence)
+	if err == syscall.EINTR {
+		goto intr
+	}
 	return off, errs.Wrap(err)
 }
 
 func (fh File) Truncate(n int64) (err error) {
-	return errs.Wrap(syscall.Ftruncate(int(fh.fd), n))
+intr:
+	err = syscall.Ftruncate(int(fh.fd), n)
+	if err == syscall.EINTR {
+		goto intr
+	}
+	return errs.Wrap(err)
 }
 
 func (fh File) Sync() (err error) {
-	return errs.Wrap(syscall.Fsync(int(fh.fd)))
+intr:
+	err = syscall.Fsync(int(fh.fd))
+	if err == syscall.EINTR {
+		goto intr
+	}
+	return errs.Wrap(err)
 }
 
-func (fh File) Fd() int { return int(fh.fd) }
-
 func (fh File) Size() (int64, error) {
+intr:
 	var stat syscall.Stat_t
-	if err := syscall.Fstat(int(fh.fd), &stat); err != nil {
+	err := syscall.Fstat(int(fh.fd), &stat)
+	if err == syscall.EINTR {
+		goto intr
+	} else if err != nil {
 		return 0, errs.Wrap(err)
 	}
 	return stat.Size, nil
 }
 
-func (fh File) Fallocate(n int64) error {
-	return errs.Wrap(syscall.Fallocate(int(fh.fd), 0, 0, n))
+func (fh File) Fallocate(n int64) (err error) {
+intr:
+	err = syscall.Fallocate(int(fh.fd), 0, 0, n)
+	if err == syscall.EINTR {
+		goto intr
+	}
+	return errs.Wrap(err)
 }
--- filesystem/filesystem.go
+++ filesystem/filesystem.go
@@ -2,6 +2,7 @@ package filesystem
 
 import (
 	"os"
+	"reflect"
 	"runtime"
 	"syscall"
 	"unsafe"
@@ -10,14 +11,22 @@ import (
 type T struct {
 }
 
+// strData returns the data pointer for the string. It does not
+// keep the string alive, so be sure to use runtime.KeepAlive.
+func strData(x string) uintptr {
+	return (*reflect.StringHeader)(unsafe.Pointer(&x)).Data
+}
+
 func (t *T) Create(path string) (fh File, err error) {
-	fd, _, ec := syscall.Syscall(
-		syscall.SYS_OPEN,
-		uintptr(*(*unsafe.Pointer)(unsafe.Pointer(&path))),
+intr:
+	fd, _, ec := syscall.Syscall(syscall.SYS_OPEN,
+		strData(path),
 		uintptr(os.O_RDWR|os.O_CREATE),
 		0644,
 	)
-	if ec != 0 {
+	if ec == syscall.EINTR {
+		goto intr
+	} else if ec != 0 {
 		return File{}, ec
 	}
 
@@ -26,13 +35,15 @@ func (t *T) Create(path string) (fh File, err error) {
 }
 
 func (t *T) Open(path string) (fh File, err error) {
-	fd, _, ec := syscall.Syscall(
-		syscall.SYS_OPEN,
-		uintptr(*(*unsafe.Pointer)(unsafe.Pointer(&path))),
+intr:
+	fd, _, ec := syscall.Syscall(syscall.SYS_OPEN,
+		strData(path),
 		uintptr(os.O_RDONLY),
 		0644,
 	)
-	if ec != 0 {
+	if ec == syscall.EINTR {
+		goto intr
+	} else if ec != 0 {
 		return File{}, ec
 	}
 
@@ -40,16 +51,17 @@ func (t *T) Open(path string) (fh File, err error) {
 	return File{fd}, nil
 }
 
-var _AT_FDCWD = -0x64
+const _AT_FDCWD = ^uintptr(0x64) + 1
 
 func (t *T) Rename(old, new string) error {
+intr:
 	_, _, ec := syscall.Syscall6(syscall.SYS_RENAMEAT,
-		uintptr(_AT_FDCWD),
-		uintptr(*(*unsafe.Pointer)(unsafe.Pointer(&old))),
-		uintptr(_AT_FDCWD),
-		uintptr(*(*unsafe.Pointer)(unsafe.Pointer(&new))),
+		_AT_FDCWD, strData(old),
+		_AT_FDCWD, strData(new),
 		0, 0)
-	if ec != 0 {
+	if ec == syscall.EINTR {
+		goto intr
+	} else if ec != 0 {
 		return ec
 	}
 
@@ -59,12 +71,13 @@ func (t *T) Rename(old, new string) error {
 }
 
 func (t *T) Remove(path string) error {
+intr:
 	_, _, ec := syscall.Syscall(syscall.SYS_UNLINKAT,
-		uintptr(_AT_FDCWD),
-		uintptr(*(*unsafe.Pointer)(unsafe.Pointer(&path))),
+		_AT_FDCWD, strData(path),
 		0)
-
-	if ec != 0 {
+	if ec == syscall.EINTR {
+		goto intr
+	} else if ec != 0 {
 		return ec
 	}
 
